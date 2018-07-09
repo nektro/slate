dca1193317cc1eea991e708866905852f04e1c1d
Meghan <nektromc@gmail.com>
2018-07-09 19:24:38 -0400

    add wast to wasm generation

--- src/slate.js
+++ src/slate.js
@@ -4,12 +4,26 @@
 import { lexer } from "./lexer.js";
 import { parser } from "./parser.js";
 import { converter } from "./converter.js";
+import "https://cdn.rawgit.com/WebAssembly/wabt/e5b3830/demo/libwabt.js";
 
 //
 export {
     lexer,
     parser,
     converter,
+}
+
+//
+const wabt_module = WabtModule();
+
+//
+export async function convert_to_wasm(text) {
+    return Promise.resolve(text)
+    .then(x => wabt_module.parseWat("test.wat", x))
+    .then(x => x.toBinary({}))
+    .then(x => x.buffer)
+    .then(x => new Blob([x], { type:"application/wasm" }))
+    .then(x => new Response(x));
 }
 
 //
@@ -20,4 +34,5 @@ export async function parse(text) {
     .then(x => parser.parse(x))
     .then(x => converter.normalize(x))
     .then(x => x.convert_to_wast())
+    .then(x => convert_to_wasm(x))
 }
