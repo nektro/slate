65d5ee04206e404652c05497f5e9a2bf7a37c806
Meghan <nektromc@gmail.com>
2018-07-09 19:13:52 -0400

    add parser

--- src/imports.js
+++ src/imports.js
@@ -4,3 +4,4 @@
 "use strict";
 //
 export * from "https://cdn.rawgit.com/nektro/basalt/1c77f17/src/lex.js";
+export * from "https://cdn.rawgit.com/nektro/basalt/1c77f17/src/parse.js";
--- src/slate.js
+++ src/slate.js
@@ -2,14 +2,18 @@
  */
 //
 import { lexer } from "./lexer.js";
+import { parser } from "./parser.js";
 
 //
 export {
     lexer,
+    parser,
 }
 
 //
 export async function parse(text) {
     return Promise.resolve(text)
     .then(x => lexer.parse(x))
+    .then(x => parser.convert_tokens_to_expressions(x))
+    .then(x => parser.parse(x))
 }
