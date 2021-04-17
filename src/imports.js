d4f488f18a4e8c793b794409f06c747c11c8e0ed
Meghan Denny <hello@nektro.net>
2021-04-17 04:24:27 -0700

    remove js compiler implementation

--- src/imports.js
+++
@@ -1,7 +0,0 @@
-/**
- */
-//
-"use strict";
-//
-export * from "https://cdn.jsdelivr.net/gh/nektro/basalt@1c77f17/src/lex.js";
-export * from "https://cdn.jsdelivr.net/gh/nektro/basalt@1c77f17/src/parse.js";
